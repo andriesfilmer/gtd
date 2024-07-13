23d5d9625b07bf6f7d42903f05dae4ac55946385
Andries Filmer <andries@filmer.nl>
2024-07-13 09:46:48 +0200

    added nvim config

---
+++ dotfiles/.config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>') -- , opts('Go pane right'))
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>') -- , opts('Go pane down'))
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>') -- , opts('Go pane left'))
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>') -- , opts('Go pane up'))
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
---
+++ dotfiles/.config/nvim/lua/core/plugin_config/which_key.lua
@@ -0,0 +1,21 @@
+require('which-key').setup({
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+})
