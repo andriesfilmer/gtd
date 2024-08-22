5c2aba2c368f79ed0e5f44185639cd0297962011
Andries Filmer <andries@filmer.nl>
2024-08-22 23:23:05 +0200

    added plugin colorizer

---
+++ dotfiles/.config/nvim/lua/lazy/plugins/colorizer.lua
@@ -0,0 +1,11 @@
+return {
+  "norcalli/nvim-colorizer.lua",
+  config = function()
+    require 'colorizer'.setup {
+      css = { rgb_fn = true, hsl_fn = true },
+      scss = { rgb_fn = true, hsl_fn = true },
+      javascript = { rgb_fn = true, hsl_fn = true },
+      html = { rgb_fn = true, hsl_fn = true, mode = 'foreground' },
+    }
+  end,
+}
